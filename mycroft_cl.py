03f428675188537c1ddcdd51017d5ea6377c293b
Andrew Morris <andy_morris22@yahoo.com>
2020-06-21 13:31:23 -0400

    adds listen test, env vars, and functionalizes main

---
+++ mycroft_cl.py
@@ -0,0 +1,52 @@
+#!/bin/env python
+import json
+from websocket import create_connection  # type: ignore
+import logging
+import sys
+import os
+
+logging.basicConfig(level=logging.DEBUG)
+try:
+    MYCROFT_ADDR_ENV = os.environ["MYCROFT_ADDR"]
+    MYCROFT_PORT_ENV = os.environ["MYCROFT_PORT"]
+    JSON_DIR_ENV = os.environ["MYCROFT_JSON_DIR"]
+    logging.info("ENV VARS SET")
+except KeyError:
+    logging.warning("ENV VARS NOT SET. Using default.")
+    MYCROFT_ADDR_ENV = "localhost"
+    MYCROFT_PORT_ENV = "8181"
+
+
+def send_message(message, mycroft_addr=MYCROFT_ADDR_ENV, mycroft_port=MYCROFT_PORT_ENV):
+    url = f"ws://{mycroft_addr}:{mycroft_port}/core"
+    logging.info(f"Websocket url: {url}")
+    ws = create_connection(url)
+    # try
+    logging.info(f"String sent: {message}")
+    send_status = ws.send(json.dumps(message))
+    logging.info(f"Send status: {send_status}")
+    result = ws.recv()
+    logging.info(f"received: {result}")
+    # except:
+    logging.warning("Failed")
+    ws.close()
+
+
+def get_mycroft_message(command, json_dir=JSON_DIR_ENV):
+    json_file = f"{json_dir}/{command}.json"
+    logging.debug(f"json_file: {json_file}")
+    with open(f"{json_file}", "rb") as fh:
+        message = json.load(fh)
+    logging.debug(f"json_message: {message}")
+    return message
+
+
+def run(command, mycroft_addr=MYCROFT_ADDR_ENV, mycroft_port=MYCROFT_PORT_ENV):
+    message = get_mycroft_message(command)
+    if command.lower() == "listen":
+        pass
+    send_message(message, mycroft_addr, mycroft_port)
+
+
+if __name__ == "__main__":
+    run(sys.argv[1])
---
+++ test_mycroft-cli.py
@@ -0,0 +1,23 @@
+# /bin/env python3
+
+import unittest
+import websocket
+import mock
+import mycroft_cl
+import os
+
+
+class test_speak_subcommand(unittest.TestCase):
+    def setUp(self):
+        self.MYCROFT_PORT_OLD = os.environ["MYCROFT_PORT"]
+        self.MYCROFT_ADDR_OLD = os.environ["MYCROFT_ADDR"]
+        os.environ["MYCROFT_PORT"] = "8181"
+        os.environ["MYCROFT_ADDR"] = "localhost"
+
+    def test_speak_subcommand(self):
+        with unittest.mock.patch(mycroft_cl.create_connection):
+            mycroft_cl.run("speak")
+
+    def tearDown(self):
+        os.environ["MYCROFT_PORT"] = self.MYCROFT_PORT_OLD
+        os.environ["MYCROFT_ADDR"] = self.MYCROFT_ADDR_OLD
